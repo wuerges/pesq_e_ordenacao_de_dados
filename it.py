c12195360c494fe13920ff873af8f454039cec2a
Emilio Wuerges <wuerges@gmail.com>
2018-10-26 16:31:16 -0300

    yay

--- it.py
+++ it.py
@@ -1,5 +1,5 @@
 
-identity = 0
+identity = int(1e8)
 
 class P:
     def __init__(self, x, y):
@@ -54,71 +54,59 @@ class IT:
         if i.p1.lt(self.i.p1) and self.i.p2.lt(i.p2):
             print("lazy", self, i, v)
             self.lazy = (True, v)
-            return
-        if self.i.cols(i):
+            self.push()
+        elif self.i.cols(i):
+            self.push()
             print("collides", self, i, v)
-            if self.i.p1 == self.i.p2:
-                self.v = v
-                return
-            else:
-                self.get_q1().update(i, v)
-                self.get_q2().update(i, v)
-                self.get_q3().update(i, v)
-                self.get_q4().update(i, v)
 
+            self.fix_ptrs()
+            self.q1.update(i, v)
+            self.q2.update(i, v)
+            self.q3.update(i, v)
+            self.q4.update(i, v)
 
-    def push(self):
-        (l, v) = self.lazy
-        self.lazy = (False, 0)
-        if l:
-            if self.i.p1 == self.i.p2:
-                self.v = v
-            else:
-                self.get_q1().update(self.i, v)
-                self.get_q2().update(self.i, v)
-                self.get_q3().update(self.i, v)
-                self.get_q4().update(self.i, v)
+            self.v = min(self.q1.v, self.q2.v, self.q3.v, self.q4.v)
 
-
-    def query(self, i):
-        self.push()
-
-        if not self.i.cols(i):
-            return identity
-        else:
-            print("query", self, i)
-            if self.i.p1 == self.i.p2:
-                return self.v
-            else:
-                r = identity
-                r = max(self.get_q1().query(i), r)
-                r = max(self.get_q2().query(i), r)
-                r = max(self.get_q3().query(i), r)
-                r = max(self.get_q4().query(i), r)
-                return r
-
-
-    def get_q1(self):
+    def fix_ptrs(self):
         if not self.q1:
             self.q1 = IT(self.i.p1, self.mid)
-        return self.q1
 
-    def get_q2(self):
         if not self.q2:
             p1 = P(self.mid.x+1, self.i.p1.y)
             p2 = P(self.i.p2.x, self.mid.y)
             self.q2 = IT(p1, p2)
-        return self.q2
 
-    def get_q3(self):
         if not self.q3:
-
             self.q3 = IT(P(self.mid.x+1, self.mid.y+1), self.i.p2)
-        return self.q3
 
-    def get_q4(self):
         if not self.q4:
             p1 = P(self.i.p1.x, self.mid.y+1)
             p2 = P(self.mid.x, self.i.p2.y)
             self.q4 = IT(p1, p2)
-        return self.q4
+
+
+    def push(self):
+        (l, v) = self.lazy
+        if l:
+            self.lazy = (False, 0)
+            self.v = v
+            if not self.i.p1 == self.i.p2:
+                self.fix_ptrs()
+                self.q1.lazy = (l, v)
+                self.q2.lazy = (l, v)
+                self.q3.lazy = (l, v)
+                self.q4.lazy = (l, v)
+
+
+    def query(self, i):
+        if not self.i.cols(i):
+            return identity
+
+        (l, v) = self.lazy
+        if l:
+            return v
+        if i.p1.lt(self.i.p1) and self.i.p2.lt(i.p2):
+            return self.v
+        self.fix_ptrs()
+
+        return min(self.q1.query(i), self.q2.query(i), self.q3.query(i), self.q4.query(i))
